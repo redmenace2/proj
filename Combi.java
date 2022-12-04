06022b0439dd931e18f5f1b43b4cb48f349ecea1
redmenace2 <111321222+redmenace2@users.noreply.github.com>
2022-12-04 11:46:58 +1100

    Update Combi.java

--- Combi.java
+++ Combi.java
@@ -1,70 +1,18 @@
-package combi;
 
-
-import java.util.ArrayList;
-
-	import java.util.List;
-	
 public class Combi {
 
-		int n, m=0;
-
-		int f=0;
-		int i=0;
-
-		 int [] ns  = {1,2,3,4,5};
-		
-		int L = ns.length;
-		
+		int [] ns  = {1,2,3,4,5};
 		ArrayList<ArrayList<Integer>> B = new ArrayList<>();
 		ArrayList  <Integer> A = new ArrayList<>();
 
 		void R (int r, int n, ArrayList <Integer>C )
-		
-		{	System.out.println("__LOOP_BEGIN__");
-			
-			for (int i=1; i<= ns.length; i++)
-				
+		{for (int i=1; i<= ns.length; i++)	
 			{ 	
-//				n=n+1;
-//				System.out.println(n+ " i " + i);
-				
-				C.add(i);
-				
-				if (C.size()<ns.length) {R (r,r,C);}
-		
-	
-				if (C.size() == ns.length)
-					{B.add((ArrayList<Integer>) C.clone());}
-				
-				System.out.println(C);
-				
-				C.remove(C.size()-1);
-	
-			
+			C.add(i);
+			if (C.size()<ns.length) {R (r,r,C);}
+			if (C.size() == ns.length) {B.add((ArrayList<Integer>) C.clone());}
+			C.remove(C.size()-1);
 			}
-			
-			System.out.println("****LOOP*END*****");
-			
-		
-			System.out.println();
-		}
-		
-		public static void main (String arg[])
-		
-		
-		{ ArrayList <Integer> A= new ArrayList<>();
-		
-		Combi R = new Combi();
-		
-		R.R(0,0, A);
-			
-		for (int k=0; k<=(R.B.size()-1); k++)
-			
-		{
-			System.out.println(R.B.get(k));
-			
 		}
 		}
-
-	}
+		
